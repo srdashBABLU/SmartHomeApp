6e3836fac23ca9904f21fbe010f166db1a4d44e2
Soumya Ranjan dash <137494629+srdashbablu@users.noreply.github.com>
2025-01-24 18:52:21 +0530

    Create README.md

--- app/src/main/java/com/example/smarthomeapp/presentation/views/HomeScreenView.kt
+++ app/src/main/java/com/example/smarthomeapp/presentation/views/HomeScreenView.kt
@@ -1,8 +1,22 @@
 package com.example.smarthomeapp.presentation.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun homeScreenPrev(){
+    HomeScreen()
 }
